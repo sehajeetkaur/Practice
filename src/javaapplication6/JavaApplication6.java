67b9de5a791048937220dee1f12919eee477bad8
sehajeetkaur <kasehaje@sheridancollege.ca>
2024-06-01 10:05:14 -0400

    This is my final file and i am going to commit.

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        /** i know i can do this **/
+        // TODO code application logic here
+    }
+    
+}
